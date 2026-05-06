df43abec9cf072641a8f8db48f1be9e920da2fa0
MURERA <mrezar531@gmail.com>
2026-05-06 22:24:09 +0700

    final deployment and security update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3yg+#e%%md8yrk1%z-yd22^dse3*k+=s6(x#(fw(k!%4_$fk-2'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
