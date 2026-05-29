5c141259bd48988d6bc72535109d8b4a1530ad10
MURERA <mrezar531@gmail.com>
2026-05-29 16:42:13 +0700

    add cors allowed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -176,5 +176,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://frontendcstone.onrender.com",
+    "https://capstone-fullstackb11.onrender.com",
 ]
 
